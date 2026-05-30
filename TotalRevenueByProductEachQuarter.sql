bfda22e2e040c60efd2131107d71c8ee3815f125
Kieran McGee <mcgeekieran9@gmail.com>
2026-05-30 17:56:52 +0100

    Added first SQL Query for total revenue by product for each quarter

---
+++ TotalRevenueByProductEachQuarter.sql
@@ -0,0 +1,22 @@
+WITH cte AS (
+	SELECT 
+		CONCAT(YEAR(s.sale_date), ' Q', QUARTER(s.sale_date)) sales_quarter,
+        p.Product_Name, p.Price,
+        SUM(s.quantity) total_quantity
+	FROM products p
+		JOIN sales s ON p.Product_ID = s.product_id
+GROUP BY
+	CONCAT(YEAR(s.sale_date), ' Q', QUARTER(s.sale_date)),
+    p.Product_ID,
+    P.Product_Name,
+    p.Price)
+
+SELECT 
+	sales_quarter,
+	Product_Name,
+    Price, total_quantity,
+    price*total_quantity AS total_revenue
+FROM cte
+ORDER BY 
+	sales_quarter, 
+	Product_Name ASC;
