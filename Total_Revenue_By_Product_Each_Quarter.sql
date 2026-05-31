fdb46aa7cb1496fb9d961d1311484507f969c298
Kieran McGee <mcgeekieran9@gmail.com>
2026-05-31 16:59:43 +0100

    Updated query for total revenue per product each quarter to include the product category

--- Total_Revenue_By_Product_Each_Quarter.sql
+++ Total_Revenue_By_Product_Each_Quarter.sql
@@ -2,21 +2,25 @@ WITH cte AS (
 	SELECT 
 		CONCAT(YEAR(s.sale_date), ' Q', QUARTER(s.sale_date)) sales_quarter,
         p.Product_Name, 
+        c.category_name,
         p.Price,
         SUM(s.quantity) total_quantity
 	FROM products p
-		JOIN sales s ON p.Product_ID = s.product_id
+	JOIN sales s ON p.Product_ID = s.product_id
+	JOIN category c ON c.category_id = p.Category_ID
 GROUP BY
 	CONCAT(YEAR(s.sale_date), ' Q', QUARTER(s.sale_date)),
-    p.Product_ID,
     P.Product_Name,
+    c.category_name,
     p.Price)
 
 SELECT 
 	sales_quarter,
 	Product_Name,
+    category_name,
     Price, total_quantity,
     price*total_quantity AS total_revenue
+    
 FROM cte
 ORDER BY 
 	sales_quarter, 
